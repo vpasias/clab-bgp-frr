af8fe6cf2c49b6cbf61d7bfcf69b7c7015bdedbd
vpasias <52623837+vpasias@users.noreply.github.com>
2022-11-10 13:24:57 +0200

    Update host_connect_test.sh

--- host_connect_test.sh
+++ host_connect_test.sh
@@ -11,3 +11,4 @@ docker exec clab-bgp_frr-leaf2 ping 192.168.22.2 -I 192.168.21.1 -c 1
 docker exec clab-bgp_frr-leaf2 ping 192.168.31.2 -I 192.168.21.1 -c 1
 docker exec clab-bgp_frr-leaf2 ping 192.168.32.2 -I 192.168.21.1 -c 1
 docker exec clab-bgp_frr-leaf3 ping 192.168.32.2 -I 192.168.31.1 -c 1
+docker exec clab-bgp_frr-leaf4 ping 192.168.42.2 -I 192.168.41.1 -c 1
