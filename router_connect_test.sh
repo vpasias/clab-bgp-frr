84d39f84fe90988f7a59232c556753de575d9749
vpasias <52623837+vpasias@users.noreply.github.com>
2022-11-11 11:20:10 +0200

    Update router_connect_test.sh

--- router_connect_test.sh
+++ router_connect_test.sh
@@ -8,4 +8,4 @@ docker exec clab-bgp_frr-leaf2 ping 192.168.22.2 -c 1
 docker exec clab-bgp_frr-leaf3 ping 192.168.31.2 -c 1
 docker exec clab-bgp_frr-leaf3 ping 192.168.32.2 -c 1
 docker exec clab-bgp_frr-leaf4 ping 192.168.41.2 -c 1
-
+docker exec clab-bgp_frr-leaf4 ping 192.168.255.1 -c 1
