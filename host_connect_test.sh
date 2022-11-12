ae8426e026e0b25489fe9fbec521888f976e4d51
vpasias <52623837+vpasias@users.noreply.github.com>
2022-11-12 12:54:25 +0200

    Update host_connect_test.sh

--- host_connect_test.sh
+++ host_connect_test.sh
@@ -11,4 +11,3 @@ docker exec clab-lf-leaf2 ping 192.168.22.2 -I 192.168.21.1 -c 1
 docker exec clab-lf-leaf2 ping 192.168.31.2 -I 192.168.21.1 -c 1
 docker exec clab-lf-leaf2 ping 192.168.32.2 -I 192.168.21.1 -c 1
 docker exec clab-lf-leaf3 ping 192.168.32.2 -I 192.168.31.1 -c 1
-docker exec clab-lf-leaf4 ping 192.168.41.2 -I 192.168.41.1 -c 1
