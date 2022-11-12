c319cb3c0de664347154b89420205ca24e7cab39
vpasias <52623837+vpasias@users.noreply.github.com>
2022-11-12 10:49:58 +0200

    Update router_connect_test.sh

--- router_connect_test.sh
+++ router_connect_test.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 # Test host-leaf connectivity
-docker exec clab-bgp_frr-leaf1 ping 192.168.11.2 -c 1
-docker exec clab-bgp_frr-leaf1 ping 192.168.12.2 -c 1
-docker exec clab-bgp_frr-leaf2 ping 192.168.21.2 -c 1
-docker exec clab-bgp_frr-leaf2 ping 192.168.22.2 -c 1
-docker exec clab-bgp_frr-leaf3 ping 192.168.31.2 -c 1
-docker exec clab-bgp_frr-leaf3 ping 192.168.32.2 -c 1
-docker exec clab-bgp_frr-leaf4 ping 192.168.41.2 -c 1
-docker exec clab-bgp_frr-leaf4 ping 192.168.255.1 -c 1
+docker exec clab-lf-leaf1 ping 192.168.11.2 -c 1
+docker exec clab-lf-leaf1 ping 192.168.12.2 -c 1
+docker exec clab-lf-leaf2 ping 192.168.21.2 -c 1
+docker exec clab-lf-leaf2 ping 192.168.22.2 -c 1
+docker exec clab-lf-leaf3 ping 192.168.31.2 -c 1
+docker exec clab-lf-leaf3 ping 192.168.32.2 -c 1
+docker exec clab-lf-leaf4 ping 192.168.41.2 -c 1
+docker exec clab-lf-leaf4 ping 192.168.255.1 -c 1
